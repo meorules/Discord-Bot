4d95ee4b33c10e28134091f4e066c3e6c1f6ede5
Mazen Omar <b9042121@my.shu.ac.uk>
2023-03-18 03:37:39 +0000

    Fixed Deploy Commands to deploy to both servers

--- deploy-commands.js
+++ deploy-commands.js
@@ -24,12 +24,22 @@ const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
 
         // The put method is used to fully refresh all commands in the guild with the current set
         const data = await rest.put(
+            Routes.applicationGuildCommands(APP_ID, FD_SERVER_ID), { body: commands },
+        );
+
+        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+    } catch (error) {
+        // And of course, make sure you catch and log any errors!
+        console.error(error);
+    }
+
+    try {
+        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+
+        // The put method is used to fully refresh all commands in the guild with the current set
+        const data = await rest.put(
             Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body: commands },
         );
-        // const data = await rest.put(
-        //     Routes.applicationGuildCommands(APP_ID, FD_SERVER_ID), { body: commands },
-        // );
-
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
         // And of course, make sure you catch and log any errors!
