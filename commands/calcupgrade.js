7faa51eb97b44b1c40eaaafa8f4e6ee1efe73b55
Mazen Omar <b9042121@my.shu.ac.uk>
2023-03-18 01:49:27 +0000

    Added CalcUpgrade Command

---
+++ commands/calcupgrade.js
@@ -0,0 +1,221 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+
+
+// Rating
+// To stop the best players developing at the same rate as lower rated players we have added a system for those of that quality, as follows:
+
+// Team of the Season
+// Please apply these upgrades after other upgrades have been completed:
+// • 88+ = +0
+// • 84-87 = +1
+// • 81-83 = +2
+// • 77-80 = +3
+// • <76 = +4
+// Rest of Squad
+// Upgrades and Downgrades for players who made 9 or less appearances. These Upgrades and Downgrades affect the player in every position.
+
+function calcTOTSUpgrade(overall) {
+    if (overall <= 88) {
+        return 0;
+    } else if (overall >= 84 && overall <= 87) {
+        return 1;
+    } else if (overall >= 81 && overall <= 83) {
+        return 2;
+    } else if (overall >= 77 && overall <= 80) {
+        return 3;
+    } else if (overall <= 76) {
+        return 4;
+    }
+}
+
+function starterUpgrades(age, growth, averageRating) {
+    let upgrade = 0;
+    if (growth == 0) {
+        if (age => 33) {
+            if (averageRating >= 76) {
+                upgrade = 0;
+            } else if (averageRating >= 69 && averageRating <= 75) {
+                upgrade = -1;
+            } else if (averageRating >= 64 && averageRating <= 68) {
+                upgrade = -2;
+            } else if (averageRating >= 57 && averageRating <= 63) {
+                upgrade = -3;
+            } else if (averageRating <= 56) {
+                upgrade = -4;
+            }
+        } else if (age <= 32 && age >= 30) {
+            if (averageRating >= 71) {
+                upgrade = 1;
+            } else if (averageRating >= 65 && averageRating <= 70) {
+                upgrade = 0;
+            } else if (averageRating >= 60 && averageRating <= 64) {
+                upgrade = -1;
+            } else if (averageRating >= 56 && averageRating <= 59) {
+                upgrade = -2;
+            } else if (averageRating <= 55) {
+                upgrade = -3;
+            }
+        } else if (age < 30) {
+            if (averageRating >= 71) {
+                upgrade = 1;
+            } else if (averageRating >= 63 && averageRating <= 70) {
+                upgrade = 0;
+            } else if (averageRating >= 58 && averageRating <= 62) {
+                upgrade = -1;
+            } else if (averageRating >= 55 && averageRating <= 57) {
+                upgrade = -2;
+            } else if (averageRating <= 54) {
+                upgrade = -3;
+            }
+        }
+    } else if (growth == 1 || growth == 2) {
+        if (averageRating >= 75) {
+            upgrade = 2;
+        } else if (averageRating >= 68 && averageRating <= 74) {
+            upgrade = 1;
+        } else if (averageRating >= 60 && averageRating <= 67) {
+            upgrade = 0;
+        } else if (averageRating >= 55 && averageRating <= 59) {
+            upgrade = -2;
+        } else if (averageRating <= 54) {
+            upgrade = -3;
+        }
+
+    } else if (growth == 3 || growth == 4 || growth == 6) {
+        if (averageRating >= 80) {
+            upgrade = 3;
+        } else if (averageRating >= 70 && averageRating <= 79) {
+            upgrade = 2;
+        } else if (averageRating >= 60 && averageRating <= 69) {
+            upgrade = 1;
+        } else if (averageRating >= 55 && averageRating <= 59) {
+            upgrade = 0;
+        } else if (averageRating <= 54) {
+            upgrade = -1;
+        }
+
+    } else if (growth => 7 && growth <= 11) {
+        if (averageRating >= 80) {
+            upgrade = 4;
+        } else if (averageRating >= 70 && averageRating <= 79) {
+            upgrade = 3;
+        } else if (averageRating >= 65 && averageRating <= 69) {
+            upgrade = 2;
+        } else if (averageRating >= 60 && averageRating <= 64) {
+            upgrade = 1;
+        } else if (averageRating >= 55 && averageRating <= 59) {
+            upgrade = 0;
+        } else if (averageRating <= 54) {
+            upgrade = -1;
+        }
+
+    } else if (growth => 12) {
+        if (averageRating >= 80) {
+            upgrade = 5;
+        } else if (averageRating >= 70 && averageRating <= 79) {
+            upgrade = 4;
+        } else if (averageRating >= 65 && averageRating <= 69) {
+            upgrade = 3;
+        } else if (averageRating >= 60 && averageRating <= 64) {
+            upgrade = 2;
+        } else if (averageRating >= 55 && averageRating <= 59) {
+            upgrade = 2;
+        } else if (averageRating <= 54) {
+            upgrade = 1;
+        }
+
+    }
+
+    return upgrade;
+
+}
+
+function backupUpgrades(age) {
+    if (age >= 33) {
+        return -3;
+    } else if (age >= 31 && age <= 32) {
+        return -2;
+    } else if (age >= 28 && age <= 30) {
+        return -1;
+    } else if (age >= 24 && age <= 27) {
+        return 1;
+    } else if (age >= 20 && age <= 23) {
+        return 2;
+    } else if (age >= 16 && age <= 19) {
+        return 4;
+    }
+
+}
+
+
+function calcPositionAverage(position, averageRating) {
+    if (position == "GK" || position == "CB" || position == "RB" || position == "LB" || position == "LWB" || position == "RWB" || position == "CDM") {
+        return averageRating + 6;
+    } else if (position == "CM") {
+        return averageRating + 5;
+    } else if (position == "CAM" || position == "LM" || position == "RM") {
+        return averageRating + 3;
+    } else {
+        return averageRating;
+    }
+
+}
+
+function highRatedPlayerCalculator(rating, averageRating) {
+    minusRating = 0;
+
+    if (rating >= 91) {
+        minusRating = -5;
+    } else if (rating <= 90 && rating >= 88) {
+        minusRating = -3;
+    } else if (rating <= 87 && rating >= 85) {
+        minusRating = -2;
+    } else if (rating <= 84 && rating >= 82) {
+        minusRating = -1;
+    }
+
+    return averageRating + minusRating;
+
+}
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('calcupgrade')
+        .setDescription("Calculate a single player's new overall, potential and age:)")
+        .addBooleanOption(option => option.setName('tots').setDescription('Put 1 if they are in the TOTS and 0 if not'))
+        .addBooleanOption(option => option.setName('starter').setDescription('Put 1 if they played 10 or more games and 0 if they played less than 10 games.'))
+        .addIntegerOption(option => option.setName('overall').setDescription("The player's overall rating before boosts"))
+        .addIntegerOption(option => option.setName('potential').setDescription("The player's potential rating before boosts"))
+        .addStringOption(option => option.setName('position').setDescription("The player's position during the sim, such as LW, RW, ST, CF, CAM, etc"))
+        .addIntegerOption(option => option.setName('averagerating').setDescription("The average rating of the player before positional modifiers, but with training added. ex. 76 or 68"))
+        .addIntegerOption(option => option.setName('age').setDescription('The age of the player)')),
+    async execute(interaction) {
+        overall = interaction.options.getInteger('overall');
+        potential = interaction.options.getInteger('potential');
+        position = interaction.options.getString('position');
+        if (interaction.options.getBoolean('starter') == True) {
+            growth = potential - overall;
+            if (overall >= 82) {
+                averageRating = highRatedPlayerCalculator(overall, interaction.options.getInteger('averagerating'));
+            } else {
+                averageRating = interaction.options.getInteger('averagerating');
+            }
+            positionAverage = calcPositionAverage(position, averageRating);
+            overallAddition = starterUpgrades(interaction.options.getInteger('age'), growth, positionAverage);
+            finalOverall = overallAddition + overall;
+            if (interaction.options.getBoolean('tots') == True) {
+                finalOverall = finalOverall + calcTOTSUpgrade(finalOverall)
+            }
+        } else {
+            overallAddition = backupUpgrades(interaction.options.getInteger('age'));
+            finalOverall = overallAddition + overall;
+
+        }
+        if (potential <= finalOverall) {
+            potential = finalOverall;
+        }
+        age = age + 1;
+
+        await interaction.reply({ content: `Name? \`${position}\` \`${finalOverall}\`/\`${potential}\` \`${age}\`` });
+    },
+};
--- deploy-commands.js
+++ deploy-commands.js
@@ -23,12 +23,12 @@ const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
         console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
         // The put method is used to fully refresh all commands in the guild with the current set
-        // const data = await rest.put(
-        //     Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body: commands },
-        // );
         const data = await rest.put(
-            Routes.applicationGuildCommands(APP_ID, FD_SERVER_ID), { body: commands },
+            Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body: commands },
         );
+        // const data = await rest.put(
+        //     Routes.applicationGuildCommands(APP_ID, FD_SERVER_ID), { body: commands },
+        // );
 
         console.log(`Successfully reloaded ${data.length} application (/) commands.`);
     } catch (error) {
