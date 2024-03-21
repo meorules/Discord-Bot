972ed2c51319ea8a93c9fc5b04f2ba3b547af048
Mazen Omar <Mazeneomar@gmail.com>
2024-03-21 10:59:06 +0000

    Fixed Randomise

--- commands/randomise.js
+++ commands/randomise.js
@@ -1,46 +1,67 @@
-const { SlashCommandBuilder } = require('discord.js');
+const {
+  SlashCommandBuilder,
+  ContextMenuCommandAssertions,
+} = require("discord.js");
 
 function generateRandomNumber(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored + 1 - minCeiled) + minCeiled);
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored + 1 - minCeiled) + minCeiled);
 }
 
 function generateRandomNumbers(amount, min, max) {
-    numbers[amount];
-    for (let i = 0; i < amount; i++) {
-        numbers[i] = generateRandomNumber(min, max);
-    }
-    return numbers;
+  let numbers = [];
+  for (let i = 0; i < amount; i++) {
+    numbers[i] = generateRandomNumber(min, max);
+  }
+  return numbers;
 }
 
 function rngString(amount, min, max) {
-    numbers = generateRandomNumbers(amount, min, max);
-    size = numbers.size();
-    generatedString = size + " numbers were generated between " + min + " and " + max + ". The numbers are: ";
-    for (let i = 0; i < size; i++) {
-        generatedString += numbers[i] + ",";
-    }
+  numbers = generateRandomNumbers(amount, min, max);
+  size = numbers.length;
+  generatedString =
+    size +
+    " numbers were generated between " +
+    min +
+    " and " +
+    max +
+    ". The numbers are: ";
+  for (let i = 0; i < size; i++) {
+    generatedString = generatedString + numbers[i] + ", ";
+  }
+  generatedString = generatedString.slice(0, generatedString.length - 2);
 
-    return generatedString;
-
+  return generatedString;
 }
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('randomise')
-        .setDescription('Get the avatar URL of the selected user, or your own avatar.')
-        .addIntegerOption(option => option.setName('min').setDescription('The minimum number to generate (inclusive)'))
-        .addIntegerOption(option => option.setName('max').setDescription('The maximum number to generate (inclusive)'))
-        .addIntegerOption(option => option.setName('count').setDescription('The number of numbers to generate')),
-    async execute(interaction) {
-        min = interaction.options.getInteger('min');
-        max = interaction.options.getInteger('max');
-        count = interaction.options.getInteger('count');
+  data: new SlashCommandBuilder()
+    .setName("randomise")
+    .setDescription(
+      "Get the avatar URL of the selected user, or your own avatar.",
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("min")
+        .setDescription("The minimum number to generate (inclusive)"),
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("max")
+        .setDescription("The maximum number to generate (inclusive)"),
+    )
+    .addIntegerOption((option) =>
+      option
+        .setName("count")
+        .setDescription("The number of numbers to generate"),
+    ),
+  async execute(interaction) {
+    min = interaction.options.getInteger("min");
+    max = interaction.options.getInteger("max");
+    count = interaction.options.getInteger("count");
 
-        rngedString = rngString(count, min, max);
-
-        const user = interaction.options.getUser('target');
-        return interaction.reply(`${rngedString}`);
-    },
+    const rngedString = rngString(count, min, max);
+    return interaction.reply(`${rngedString}`);
+  },
 };
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,48 +1,61 @@
-const { REST, Routes } = require('discord.js');
-const { APP_ID, GUILD_ID, FD_SERVER_ID, CARD_FL_ID, BOT_TOKEN } = require('./config.json');
-const fs = require('node:fs');
-const path = require('node:path');
+const { REST, Routes } = require("discord.js");
+const {
+  APP_ID,
+  GUILD_ID,
+  FD_SERVER_ID,
+  CARD_FL_ID,
+  BOT_TOKEN,
+} = require("./config.json");
+const fs = require("node:fs");
+const path = require("node:path");
 
 const commands = [];
 // Grab all the command files from the commands directory you created earlier
-const commandsPath = path.join(__dirname, 'commands');
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
 
 // Grab the SlashCommandBuilder#toJSON() output of each command's data for deployment
 for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    commands.push(command.data.toJSON());
+  const command = require(`./commands/${file}`);
+  commands.push(command.data.toJSON());
 }
 
 // Construct and prepare an instance of the REST module
-const rest = new REST({ version: '10' }).setToken(BOT_TOKEN);
+const rest = new REST({ version: "10" }).setToken(BOT_TOKEN);
 
 // and deploy your commands!
-(async() => {
-    try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+(async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            Routes.applicationGuildCommands(APP_ID, CARD_FL_ID), { body: commands },
-        );
+    // The put method is used to fully refresh all commands in the guild with the current set
+    const data = await rest.put(
+      Routes.applicationGuildCommands(APP_ID, CARD_FL_ID),
+      { body: commands },
+    );
 
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        // And of course, make sure you catch and log any errors!
-        console.error(error);
-    }
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (error) {
+    // And of course, make sure you catch and log any errors!
+    console.error(error);
+  }
 
-    try {
-        console.log(`Started refreshing ${commands.length} application (/) commands.`);
+  // try {
+  //     console.log(`Started refreshing ${commands.length} application (/) commands.`);
 
-        // The put method is used to fully refresh all commands in the guild with the current set
-        const data = await rest.put(
-            Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body: commands },
-        );
-        console.log(`Successfully reloaded ${data.length} application (/) commands.`);
-    } catch (error) {
-        // And of course, make sure you catch and log any errors!
-        console.error(error);
-    }
+  //     // The put method is used to fully refresh all commands in the guild with the current set
+  //     const data = await rest.put(
+  //         Routes.applicationGuildCommands(APP_ID, GUILD_ID), { body: commands },
+  //     );
+  //     console.log(`Successfully reloaded ${data.length} application (/) commands.`);
+  // } catch (error) {
+  //     // And of course, make sure you catch and log any errors!
+  //     console.error(error);
+  // }
 })();
