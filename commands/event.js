1ed2e9fede59a10d082823cd24326f63ceea2239
Mazen Omar <b9042121@my.shu.ac.uk>
2024-03-30 05:54:08 +0000

    Small changes to fix event message

--- commands/event.js
+++ commands/event.js
@@ -6,7 +6,7 @@ module.exports = {
         .setDescription('Setup an event with reactions'),
     async execute(interaction) {
         const r = await interaction.guild.roles.fetch("1170427348869447741");
-        messageString = `${r} \n React for event \n :newspaper: Scattegories \n :paintbrush: Regular Skribbl \n :soccer: Club Skribbl \n :map: Geoguesser \n :gun: Warzone \n:green_square: Tic-Tac-Toe \n :red_circle: Connect-4 \n :snake: Curve \n :laughing: Meme \n :money_with_wings: Monopoly \n :question: Codenames \n :hamster: Hax \n :garlic: Gartic \n :black_joker: Cards Against Humanity \n   :golf: Putt Party \n :sushi: Amongus \n :detective: Mafia \n :bat: Unknown \n :race_car: Super Karts \n :robot: Roblox`;
+        messageString = `${r} \n React for event \n :newspaper: Scattegories \n :paintbrush: Regular Skribbl \n :soccer: Club Skribbl \n :map: Geoguesser \n :gun: Warzone \n:green_square: Tic-Tac-Toe \n :red_circle: Connect-4 \n :snake: Curve \n :laughing: Meme \n :money_with_wings: Monopoly \n :question: Codenames \n :hamster: Hax \n :garlic: Gartic \n :black_joker: Cards Against Humanity \n   :golf: Putt Party \n :sushi: Amongus \n :detective: Mafia \n :bat: Unknown \n :race_car: Smash Karts \n :robot: Roblox`;
         const message = await interaction.channel.send({ content: messageString, fetchReply: true });
         interaction.reply("Setting up Event Menu and Tagged Manager Role");
         Promise.all([
